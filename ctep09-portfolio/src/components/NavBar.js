be153f1574a39e8b06460b39863386357d4a2ab3
Cassie T <cassandray9@gmail.com>
2023-05-29 18:41:37 -0600

    Portfolio assets added, Portfolio cards created with project information

--- ctep09-portfolio/src/components/NavBar.js
+++ ctep09-portfolio/src/components/NavBar.js
@@ -31,7 +31,7 @@ function NavBar({currentPage, handlePageChange}) {
       </li>
       <li>
         <a
-          href="Portfolio"
+          href="#Portfolio"
           onClick={() => handlePageChange("Portfolio")}
           className={
             currentPage === "Portfolio"
@@ -44,7 +44,7 @@ function NavBar({currentPage, handlePageChange}) {
       </li>
       <li>
         <a
-          href="Resume"
+          href="#Resume"
           onClick={() => handlePageChange("Resume")}
           className={
             currentPage === "Resume"
---
+++ ctep09-portfolio/src/components/pages/Portfolio.js
@@ -0,0 +1,72 @@
+import React from "react";
+import barista from "../images/barista.gif";
+import hopsHaven from "../images/hops-haven.png";
+import horiseon from "../images/horiseon-live-site.gif";
+
+function Portfolio() {
+  return (
+    <div className="flex flex-wrap mx-8">
+
+      {/* barista project */}
+      <div className="card mx-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 glass">
+        <figure>
+          <img
+            src={barista}
+            className="max-w-sm rounded-lg shadow-2xl"
+            alt="barista"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">barista</h2>
+          <p>
+            The barista webapp dynamically generates local cafe's, and displays
+            a list cafe's and their address.{" "}
+          </p>
+          <button className="btn btn-primary">Visit barista Site</button>
+        </div>
+      </div>
+
+      {/* Hops Haven Project */}
+      <div className="card mx-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 glass">
+        <figure>
+          <img
+            src={hopsHaven}
+            className="max-w-sm rounded-lg shadow-2xl"
+            alt="Hops Haven"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Hops Haven</h2>
+          <p>
+            Hops-Haven is a full-stack web application that beer enthusiasts can
+            use to find data on the beers they love and discover new beers they
+            might enjoy.
+          </p>
+        </div>
+      </div>
+
+      {/* Horiseon Project */}
+      <div className="card mx-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 glass">
+        <figure>
+          <img
+            src={horiseon}
+            className="max-w-sm rounded-lg shadow-2xl"
+            alt="Hops Haven"
+          />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">Hops Haven</h2>
+          <p>
+            Hops-Haven is a full-stack web application that beer enthusiasts can
+            use to find data on the beers they love and discover new beers they
+            might enjoy.
+          </p>
+        </div>
+      </div>
+
+
+    </div>
+  );
+}
+
+export default Portfolio;
