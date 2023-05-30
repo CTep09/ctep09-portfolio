b8bbb0ef8777f0c84f8afdc43a8c77bdd3ff7ebb
Cassie T <cassandray9@gmail.com>
2023-05-29 22:35:15 -0600

    Added image of resume to Resume page

--- ctep09-portfolio/src/components/pages/Resume.js
+++ ctep09-portfolio/src/components/pages/Resume.js
@@ -1,19 +1,28 @@
 import React from "react";
+import ctepperResume from "../images/ctepperResume.png";
 
 function Resume() {
   return (
-<div className="card card-side bg-base-100 shadow-xl">
-  <figure><img src="/images/stock/photo-1635805737707-575885ab0820.jpg" alt="Movie"/></figure>
-  <div className="card-body">
-    <h2 className="card-title">My Resume</h2>
-    <p>Please feel free to download a copy of my resume 📄</p>
-    <div className="card-actions justify-end">
-      <a href="https://drive.google.com/file/d/1MfZQGlk7EFF_nY22_eTxwROZnBKdSh6g/view?usp=share_link" >
-      <button className="btn btn-outline btn-info">Link to Download</button>
-      </a>
+    <div className="card card-side bg-base-100 shadow-xl">
+      <figure>
+              <img
+          src={ctepperResume}
+          alt="C Tepper Resume"
+          />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">My Resume</h2>
+        <p>Please feel free to download a copy of my resume 📄</p>
+        <div className="card-actions justify-end">
+          <a href="https://drive.google.com/file/d/1MfZQGlk7EFF_nY22_eTxwROZnBKdSh6g/view?usp=share_link">
+            <button className="btn btn-outline btn-info">
+              Link to Download
+            </button>
+          </a>
+        </div>
+      </div>
     </div>
-  </div>
-</div>
-)}
+  );
+}
 
 export default Resume;
