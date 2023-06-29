cd070cda2a242350cbb56860be56936fff7967d7
Cassie T <cassandray9@gmail.com>
2023-06-29 17:55:58 -0600

    Font updated, link to resume download updated

--- ctep09-portfolio/src/components/pages/Resume.js
+++ ctep09-portfolio/src/components/pages/Resume.js
@@ -4,12 +4,16 @@ import ctepperResume from "../images/resume.png";
 function Resume() {
   return (
     <div className="">
-      <h2 className="text-5xl font-mono">Skills</h2>
+      <h2 className="text-5xl">Skills</h2>
 
       {/* Link to Download Resume */}
       <div className="m-8">
-        <a href="https://drive.google.com/file/d/1SBZmntf_eTF50V0T-XhoGd6VX96BZp6j/view?usp=drive_link">
-          <button className="btn btn-outline btn-primary font-mono">
+        <a
+          href="https://drive.google.com/file/d/1SBZmntf_eTF50V0T-XhoGd6VX96BZp6j/view?usp=drive_link"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button className="btn btn-outline btn-primary">
             Click Here to Download My Resume
           </button>
         </a>
@@ -18,73 +22,67 @@ function Resume() {
         {/* Front End Skills Card */}
         <div className="flex flex-row card mx-4 mt-2 shadow-xl border-solid border-2 border-red-600 m-9">
           <div className="card-body">
-            <h3 className="card-title font-semibold font-mono">Front End:</h3>
-            <li className="font-semibold font-mono">HTML</li>
-            <li className="font-semibold font-mono">CSS</li>
-            <li className="font-semibold font-mono">JavaScript</li>
-            <li className="font-semibold font-mono">Bootstrap</li>
-            <li className="font-semibold font-mono">Web API</li>
-            <li className="font-semibold font-mono">jQuery</li>
-            <li className="font-semibold font-mono">AJAX</li>
-            <li className="font-semibold font-mono">Fetch</li>
-            <li className="font-semibold font-mono">JSON</li>
-            <li className="font-semibold font-mono">React | React Hooks</li>
-            <li className="font-semibold font-mono">
-              PWA (Progressive Web App)
-            </li>
-            <li className="font-semibold font-mono">Apollo GraphQL</li>
+            <h3 className="card-title font-semibold">Front End:</h3>
+            <li className="font-semibold">HTML</li>
+            <li className="font-semibold">CSS</li>
+            <li className="font-semibold">JavaScript</li>
+            <li className="font-semibold">Bootstrap</li>
+            <li className="font-semibold">Web API</li>
+            <li className="font-semibold">jQuery</li>
+            <li className="font-semibold">AJAX</li>
+            <li className="font-semibold">Fetch</li>
+            <li className="font-semibold">JSON</li>
+            <li className="font-semibold">React | React Hooks</li>
+            <li className="font-semibold">PWA (Progressive Web App)</li>
+            <li className="font-semibold">Apollo GraphQL</li>
           </div>
         </div>
 
         {/* Back End Skills Card */}
         <div className="card flex flex-row mx-4 mt-2 shadow-xl border-solid border-2 border-orange-600 m-9">
           <div className="card-body">
-            <h3 className="card-title font-semibold font-mono">Back End:</h3>
-            <li className="font-semibold font-mono">Node.js</li>
-            <li className="font-semibold font-mono">Express.js</li>
-            <li className="font-semibold font-mono">Rest API</li>
-            <li className="font-semibold font-mono">
+            <h3 className="card-title font-semibold">Back End:</h3>
+            <li className="font-semibold">Node.js</li>
+            <li className="font-semibold">Express.js</li>
+            <li className="font-semibold">Rest API</li>
+            <li className="font-semibold">
               Serverside Templating(Handlebars.js)
             </li>
-            <li className="font-semibold font-mono">Express Sessions</li>
-            <li className="font-semibold font-mono">bcrypt</li>
-            <li className="font-semibold font-mono">
-              MySQL | Relational Databses
-            </li>
-            <li className="font-semibold font-mono">Sequelize ORM</li>
-            <li className="font-semibold font-mono">
+            <li className="font-semibold">Express Sessions</li>
+            <li className="font-semibold">bcrypt</li>
+            <li className="font-semibold">MySQL | Relational Databses</li>
+            <li className="font-semibold">Sequelize ORM</li>
+            <li className="font-semibold">
               MongoDB | Non-Relational Databases
             </li>
-            <li className="font-semibold font-mono">
-              Object-Oriented Programming(OOP)
-            </li>
-            <li className="font-semibold font-mono">Unit Testing | Jest </li>
-            <li className="font-semibold font-mono">MERN Stack</li>
+            <li className="font-semibold">Object-Oriented Programming(OOP)</li>
+            <li className="font-semibold">Unit Testing | Jest </li>
+            <li className="font-semibold">MERN Stack</li>
           </div>
         </div>
 
         {/* Tools Skills Card */}
         <div className="card flex flex-row mx-4 mt-2 shadow-xl border-solid border-2 border-yellow-600 m-9">
           <div className="card-body">
-            <h3 className="card-title font-semibold font-mono">
-              Development Tools and Environments::
+            <h3 className="card-title font-semibold">
+              Development Tools and Environments:
             </h3>
-            <li className="font-semibold font-mono">
+            <li className="font-semibold">
               Developer Tools (Elements, Console, Network, Sources, Application,
               etc)
             </li>
-            <li className="font-semibold font-mono">Insomnia / Postman</li>
-            <li className="font-semibold font-mono">Git | GitHub</li>
-            <li className="font-semibold font-mono">Heroku</li>
+            <li className="font-semibold">Insomnia | Postman</li>
+            <li className="font-semibold">Git | GitHub</li>
+            <li className="font-semibold">Heroku</li>
           </div>
         </div>
 
         {/* <div className="card flex flex-row mx-4 mt-2 shadow-xl border-solid border-2 border-green-600 m-9">
           <div className="card-body">
-            <h3 className="card-title font-semibold font-mono">More:</h3>
-            <li className="font-semibold font-mono">Devops: Heroko</li>
-            <li className="font-semibold font-mono">Testing: Jest</li>
-            <li className="font-semibold font-mono">Other: Git, VSCode</li>
+            <h3 className="card-title font-semibold">More:</h3>
+            <li className="font-semibold">Devops: Heroko</li>
+            <li className="font-semibold">Testing: Jest</li>
+            <li className="font-semibold">Other: Git, VSCode</li>
           </div>
         </div> */}
       </div>
